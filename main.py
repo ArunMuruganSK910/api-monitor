54ee698074e9ce152413daa6edfc01a54bae6b4e
Arun <arunmuruganskprof@gmail.com>
2026-06-01 03:35:58 +0530

    fix CORS

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:3001"],
+    allow_origins=["http://localhost:3000", "http://localhost:3001", "https://api-monitor-frontend-b89x.onrender.com"],
     allow_methods=["*"],
     allow_headers=["*"],
     allow_credentials=True,
